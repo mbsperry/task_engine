0b1d191c7f13b74a2ad75b412d9f517038add404
Matthew Sperry <mbsperry@gmail.com>
2013-02-28 15:14:42 -0800

    Task_App v0.2
    - Removes dependence on vieditable
    - Adds task_at_index, refresh_tasks
    - Adds messagebox for editing task titles.
      - TODO: Cancel button does not work
    - TODO: Add ability to delete and add tasks, then this thing will
      essentially by complete

--- bin/task_app.rb
+++ bin/task_app.rb
@@ -8,12 +8,7 @@
 
 
 require 'rbcurse/core/util/app'
-require 'rbcurse/core/include/vieditable'
 require_relative '../lib/task_engine'
-
-class RubyCurses::List
-  include ViEditable
-end
 
 class AppHelper
 
@@ -54,6 +49,11 @@ class AppHelper
     result = @engine.update_task(task, tasklist, update_hash)
   end
 
+  def task_at_index(task_index, tasklist_index)
+    task = @engine.tasklists[tasklist_index].tasks[task_index]
+    return task
+  end
+
 end
 
 if $0 == __FILE__ then
@@ -62,6 +62,12 @@ if $0 == __FILE__ then
   App.new do
     ww = FFI::NCurses.COLS-0
     flow :width => ww, :margin_top => 1, :height => FFI::NCurses.LINES-2 do
+
+      def refresh_tasks(task_lb, tasklist_lb, app_helper)
+        curr_index = task_lb.current_index
+        task_lb.list(app_helper.get_task_titles(tasklist_lb.current_index))
+        task_lb.current_index = curr_index
+      end
 
       tasklist_lb = listbox(:list => app_helper.get_tasklist_titles, 
                             :title => "[ Tasklists ]", 
@@ -76,16 +82,26 @@ if $0 == __FILE__ then
           :title => "[ todos ]", 
           :name => "tasks", 
           :row => 1, :height => FFI::NCurses.LINES-4 )
-        @task_lb.vieditable_init_listbox
       end #stack
 
       @task_lb.bind_key(32) { |t|
         app_helper.toggle_status(t.current_index, tasklist_lb.current_index)
         curr_index = t.current_index
-        @task_lb.list(app_helper.get_task_titles(tasklist_lb.current_index))
-        @task_lb.current_index = curr_index
-      }
+        refresh_tasks(t, tasklist_lb, app_helper) 
+      } #bind_key(spacebar)
+
       @task_lb.bind_key(?e, "Edit Task") { |t|
+        # TODO: What happens if "Cancel" is chosen? 
+        cur_task = 
+          app_helper.task_at_index(t.current_index, tasklist_lb.current_index) 
+        cur_title = cur_task["title"]
+        new_title = get_string("Edit Task Title", :default => cur_title)
+        app_helper.update_at_index(
+          t.current_index,
+          tasklist_lb.current_index,
+          {"title" => new_title}
+        )
+        refresh_tasks(@task_lb, tasklist_lb, app_helper) 
       } #bind_key(e)
 
     end #flow
